ceea82bc790bc683d8bdf0fd3988e3f9973880c4
Daniel Peel <crysallis7@gmail.com>
2026-08-25 22:27:14 -0400

    Just updated the verbiage of the DM

--- utils/checkinContent.js
+++ utils/checkinContent.js
@@ -9,7 +9,7 @@ const CHECKIN_MESSAGE = `Hey! Just checking in... haven't seen you active in AFK
 
 RiffRaff is a competitive guild and we do need regular activity to keep up, but we also know people have lives and other interests outside the game, and that's completely fine. Just curious how you're doing and if there's anything the guild can help with.
 
-You can reply here with anything, or just react to this message:
+You can reply here with text and I'll share it with the guild leaders or just react to this message:
 👍 still playing, just been busy... I'll be back
 🔽 I want to keep playing but move to a less active guild
 💤 taking a bit of a break, not sure yet
